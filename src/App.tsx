5d1062f37825223608f75e0e8b283bd0c4cebadf
zhyzneuski86 <1293728@gmail.com>
2023-03-01 21:03:40 +0300

    Social network layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,57 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologi'
+
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img
+                    src='https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr'/>
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src='https://tinypng.com/images/social/website.jpg'/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>
+                    My posts
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
-
-
 
 
 export default App;
