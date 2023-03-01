a336f6f43802547854ab7c3ea769bf5b02ce00e9
zhyzneuski86 <1293728@gmail.com>
2023-03-01 21:37:27 +0300

    Add Header.tsx, Nav.tsx, Profile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Profile from "./components/Profile";
 
 
 const App = () => {
     return (
         <div className={'app-wrapper'}>
-            <header className={'header'}>
-                <img
-                    src='https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr'/>
-            </header>
-            <nav className={'nav'}>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className={'content'}>
-                <div>
-                    <img src='https://tinypng.com/images/social/website.jpg'/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                </div>
-                <div>
-                    My posts
-                </div>
-                <div>
-                    <div>
-                        Post 1
-                    </div>
-                    <div>
-                        Post 2
-                    </div>
-                </div>
+            < Header/>
+            < Nav/>
+            < Profile/>
 
-            </div>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img src='https://tinypng.com/images/social/website.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
