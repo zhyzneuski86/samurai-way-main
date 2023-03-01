8874213c17f4a64248e1027d9f302d1c8254de66
zhyzneuski86 <1293728@gmail.com>
2023-03-01 18:03:35 +0300

    add Header.tsx, Technologi.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologi'
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
+
+
+
 
 export default App;
---
+++ src/Technologi.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>Hello, samurai!</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>React</li>
+
+            </ul>
+        </div>
+    )
+}
+export default Technologies;
