bbe03f77cf182595e4aa7c9a3c6d1eed1b9b2d9b
zhyzneuski86 <1293728@gmail.com>
2023-03-10 14:10:02 +0300

    Dialogue page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import classes from './Dialogs.module.css'
- const Dialogs = () => {
+
+const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Misha
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Victor
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>How is your day?</div>
+                <div className={classes.message}>Yo</div>
+                
+            </div>
         </div>
     );
 };
